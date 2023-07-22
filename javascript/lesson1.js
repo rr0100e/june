65b2288bfabecf0719a2fb3bb2cddfba9cb4f68b
raheema <raheema_rahman@hotmail.co.uk>
2023-07-22 21:56:49 +0100

    lessson1js

---
+++ javascript/lesson1.js
@@ -0,0 +1,14 @@
+//ccomment single
+/*multi line comment
+dfhgfdfjgkg 
+djfngd
+*/
+
+console.log("hello xsssss");
+console.log("helllo world");
+var myName = "one two";
+var greeting = "heelo"
+var myName= "samia"
+
+/*empty string for space*/
+console.log(greeting+" " +myName);
