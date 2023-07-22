272800e3586397023c62e16ada2213af44d4a2fe
raheema <raheema_rahman@hotmail.co.uk>
2023-07-23 00:45:24 +0100

    homework 1 js

---
+++ javascript/h1.js
@@ -0,0 +1,29 @@
+/*string concat of two var*/
+
+var myJava="javascript is better than java"
+var myPython="and python is easy to learn"
+
+console.log(myJava+" "+myPython)
+
+/*using var create number and string*/
+
+var myNum="1"
+var myName="raheema"
+console.log(myNum)
+console.log(myName)
+
+/*date of birth with nname intro*/
+
+var name =" abdulkadir"
+var age = 24
+var dob = 1998
+
+console.log("my name is ", name, " i am years ", age, "old and i was born in ",dob)
+
+/*create var that holds string and number*/
+var myBigNum = "20"
+var myNameIs = "my name is"
+
+console.log(myBigNum)
+console.log(myNameIs)
+
